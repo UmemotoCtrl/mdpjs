83214ded16b796365fe6ad4db5a81e70c2ab5da2
UmemotoCtrl <>
2020-06-04 12:33:38 +0900

    w3c validation

--- docs/js/onlineTest.js
+++ docs/js/onlineTest.js
@@ -20,36 +20,36 @@
 	})();
 })();
 
-var mdinput;
+var mdInput;
 var article;
 var raw;
-var selecter;
+var selector;
 var radioList;
 
 let writeHTML = function () {
 	var htmlTxt
 	if ( radioNodeList.value == "mdp" ) {
-		htmlTxt = mdp(mdinput.value);
+		htmlTxt = mdp(mdInput.value);
 	} else {
-		htmlTxt = marked(mdinput.value);
+		htmlTxt = marked(mdInput.value);
 	}
 	article.innerHTML = htmlTxt;
 	raw.innerHTML = htmlTxt.replace(/</g,'&lt;').replace(/>/g,'&gt;');
 	if ( MathJax.typesetPromise )
 		MathJax.typesetPromise();
-	// console.log ( mdinput.value.replace(/\\/g,'\\\\').replace(/\n/g,'\\n') );
+	// console.log ( mdInput.value.replace(/\\/g,'\\\\').replace(/\n/g,'\\n') );
 }
 
 window.onload = function() {
 	var data = "# Comparison mdp and Marked\n\nThis is demo for mdp. For detail information, see [GitHub Repo](https://github.com/UmemotoCtrl/MarkdownParser).\n\n**You can change Markdown parser.**\n\n## Inline notation\n\n1. *em*\n1. **strong**\n1. ~~strike~~\n1. `code`\n1. [Anchor link to GitHub Repo](https://github.com/UmemotoCtrl/MarkdownParser)\n1. Inline math $\\dot{x} = f(x)$\n\n## Block notation\n\n### Table\n\n| A | B | C |\n| :---:  | ---:  | :--- |\n| $|\\alpha |$ | $|\\beta |$ ||\n| 1 || 2 |\n\n### Math formula in independent line\n\n\\[\n\\tag{1} \\dfrac{\\partial y}{\\partial x} = x\n\\]\n\n$$\n\\tag{2} f(x) :=\\begin{cases}1,\\quad &\\mbox{if}~ x\\neq 0 \\\\ 0,\\quad &\\mbox{if}~ x = 0\\end{cases}\n$$\n\n### List\n\n* a\n* b\n  1. A\n  1. B\n\n### Code block\n\n```markdown\n* a\n* b\n  1. A\n  1. B\n```\n\n### Horizontal rule\n\n---\n\n### Comment block\n\n<!--\nThis is not shown.\n-->\n\n";
-	mdinput = document.getElementById("mdinput");
+	mdInput = document.getElementById("mdInput");
 	article = document.getElementById("article");
 	raw     = document.getElementById("raw");
-	selecter= document.getElementById("selecter") ;
-	radioNodeList = selecter.parser;
+	selector= document.getElementById("selector") ;
+	radioNodeList = selector.parser;
 
-	mdinput.value = data;
+	mdInput.value = data;
 	writeHTML();
-	mdinput.oninput = writeHTML;
-	selecter.onchange = writeHTML;
+	mdInput.oninput = writeHTML;
+	selector.onchange = writeHTML;
 };
